d652c471542de3fa1cfb67bbd68ae442604687c1
MaritzaSicard <maritza.sicard@gmail.com>
2017-11-24 11:29:41 -0600

    colores para elias :)

--- Library/app/src/main/java/com/iteso/library/adapters/AdapterPublication.java
+++ Library/app/src/main/java/com/iteso/library/adapters/AdapterPublication.java
@@ -4,7 +4,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,9 +16,16 @@ import android.widget.TextView;
 
 import com.facebook.Profile;
 import com.facebook.login.widget.ProfilePictureView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.iteso.library.R;
 import com.iteso.library.beans.Notification;
 import com.iteso.library.beans.Publication;
+import com.iteso.library.common.Constants;
 import com.iteso.library.common.Utils;
 import com.iteso.library.gui.ActivityComments;
 import com.iteso.library.gui.ActivityProfile;
@@ -37,13 +46,22 @@ public class AdapterPublication extends RecyclerView.Adapter<AdapterPublication.
     private ArrayList<Publication> mDataSet;
     private Context context;
     private String id;
+    String name;
+    String idPub;
+    boolean likeButton;
+
+    private DatabaseReference mDatabaseReference = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_USER)
+            .child(id);
+
     public AdapterPublication(Context context, ArrayList mDataSet){
         this.context = context;
         this.mDataSet = mDataSet;
+        this.name = "";
     }
 
     public void setId(String id){
         this.id = id;
+        getUserName();
     }
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -52,21 +70,43 @@ public class AdapterPublication extends RecyclerView.Adapter<AdapterPublication.
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        holder.name.setText(Utils.getUserName(id));
+    public void onBindViewHolder(final ViewHolder holder, final int position) {
+        holder.name.setText(name);
         holder.comment.setText(mDataSet.get(position).getMessage());
         holder.countComments.setText(String.valueOf(mDataSet.get(position).getComments()));
         holder.countLikes.setText(String.valueOf(mDataSet.get(position).getLikes()));
         holder.photo.setProfileId(id);
+        idPub = mDataSet.get(position).getId();
+        holder.idPublication.setText(idPub);
+        getColorLikeButton();
+        if(likeButton) holder.like.setBackgroundColor(ContextCompat.getColor(context, R.color.colorPurple));
+        else holder.like.setBackgroundColor(ContextCompat.getColor(context, R.color.colorGray));
         //Falta poner el tiempo :)
         //holder.time.setText(putTime(mDataSet.get(position).getDate()));
-        //holder.photo.setProfileId(Profile.getCurrentProfile());
 
         holder.publicationComment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(view.getContext(), ActivityComments.class);
                 ((ActivityProfile)view.getContext()).startActivity(intent);
+            }
+        });
+
+        holder.like.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getColorLikeButton();
+                DatabaseReference mRef = mDatabaseReference.child(Constants.FIREBASE_USER_PUBLICATION_LIKE)
+                        .child(idPub).child(id);
+                if(likeButton){
+                    mRef.removeValue();
+                    holder.like.setBackgroundColor(ContextCompat.getColor(context, R.color.colorGray));
+                }
+                else{
+                    mRef.setValue(true);
+                    holder.like.setBackgroundColor(ContextCompat.getColor(context, R.color.colorPurple));
+                }
+
             }
         });
     }
@@ -86,8 +126,10 @@ public class AdapterPublication extends RecyclerView.Adapter<AdapterPublication.
         protected ImageView like;
         protected TextView countLikes;
         protected TextView countComments;
+        protected TextView idPublication;
         public ViewHolder(View itemView) {
             super(itemView);
+            idPublication = (TextView)itemView.findViewById(R.id.item_card_publication_card_id);
             card = (LinearLayout) itemView.findViewById(R.id.item_card_publication_card);
             name = (TextView)itemView.findViewById(R.id.item_card_publication_name);
             comment = (TextView)itemView.findViewById(R.id.item_card_publication_comment);
@@ -97,9 +139,45 @@ public class AdapterPublication extends RecyclerView.Adapter<AdapterPublication.
             like = (ImageView)itemView.findViewById(R.id.item_card_publication_like);
             countLikes = (TextView)itemView.findViewById(R.id.item_card_publication_number_likes);
             countComments = (TextView)itemView.findViewById(R.id.item_card_publication_number_comments);
-
             //poner función de los botones publicationComment y like
         }
     }
+
+    public void getUserName(){
+        DatabaseReference data = mDatabaseReference.child(Constants.FIREBASE_USER_INFO).child(Constants.FIREBASE_USER_NICKNAME);
+        data.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                name = dataSnapshot.getValue(String.class);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.v("Error", "Error al encontrar el nombre de usuario por id");
+            }
+        });
+    }
+
+    public void getColorLikeButton(){
+        Query dataRef = mDatabaseReference.child(Constants.FIREBASE_USER_PUBLICATION_LIKE)
+                .child(idPub).equalTo(id);
+        dataRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot == null){
+                    likeButton = false;
+                }
+                else{
+                    likeButton = true;
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
 
 }
